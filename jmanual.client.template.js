006a719a6a98f721ce63addabcd72d304e9fd553
Pavel R <rabeckijps@mail.ru>
2014-03-17 16:32:39 +0000

    update template file

--- jmanual.client.template.js
+++ jmanual.client.template.js
@@ -1,60 +1,76 @@
 var nextTip;
 var beginTraining;
+var closeTip;
 
 (function(){
 
-var tips = @data@;
-function setCookie(cname,cvalue,exdays)
-{
+    var tips = @data@;
+    function setCookie(cname,cvalue,exdays)
+    {
 	var d = new Date();
 	d.setTime(d.getTime()+(exdays*24*60*60*1000));
 	var expires = "expires="+d.toGMTString();
 	document.cookie = cname + "=" + cvalue + "; " + expires;
-}
+    }
 
-function getCookie(cname)
-{
+    function getCookie(cname)
+    {
 	var name = cname + "=";
 	var ca = document.cookie.split(';');
 	for(var i=0; i<ca.length; i++)
 	{
-		var c = ca[i].trim();
-		if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+	    var c = ca[i].trim();
+	    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
 	}
 	return "";
-}
+    }
 
-beginTraining = function(){
+    beginTraining = function(){
 	setCookie("jTipId", 0, 365);
 	nextTip();
-}
+    }
 
-nextTip = function () {
+    closeTip = function(){
+        $("#tipContainer").html("");
+	var idx = 1 * getCookie("jTipId");
+	if(idx >= tips.length){
+	    alert('End of training');
+            setCookie("jTipId", 0, 365);  
+	    return;
+	}
+	var tip = tips[idx];
+	if(tip.trigger === 0){
+	    nextTip();
+	}
+    }
+
+    nextTip = function () {
+	$("#tipContainer").html("");
         var nextTipId = 1 * getCookie("jTipId");
         if (nextTipId == null) {
             nextTipId = 0;
         }
-        if (showTip(nextTipId)){
-            setCookie("jTipId", 1 + nextTipId, 365);
-        } else {
+        if(nextTipId >= tips.lenght){
             alert('End of training');
-            $("#tipContainer").html("");
-            setCookie("jTipId", 0, 365);
+            setCookie("jTipId", 0, 365);  
+	    return;
+	}
+        if (showTip(tips[nextTipId])){
+            setCookie("jTipId", 1 + nextTipId, 365);
         }
-}
+    }
 
-function showTip(idx) {
-    if (tips[idx] == null)
-		return false;
-	
-    var tip = tips[idx];
+    function showTip(tip) {
+	if (tip == null){
+	    return false;
+	}
 	var firstFound = getElem(tip.selector)[0];
 	
 	if(firstFound == null){
-		alert("Element not found");
-		return false;
+	    alert("Element not found");
+	    return false;
 	}
-	
+
 	var el = firstFound.elem;
 	var position = (el == null ? null : $(el).offset());
 	position.top += firstFound.offs.top;
@@ -63,22 +79,22 @@ function showTip(idx) {
 	var tipBox = theTipTemplate({ left: position.left, top: position.top, msg: tip.msg });
 	$("#tipContainer").html(tipBox);
 	return true;
-}
+    }
 
-function showClientPanel() {
+    function showClientPanel() {
 	//Initialize templates
 	clientPanelTemplate = window['JST']['admin/templates/client-panel-template.html'];
 	theTipTemplate = window['JST']['admin/templates/tip-bubble-template.html'];
 
 	//Create DOM elements
 	$('body').append(clientPanelTemplate());
-}
+    }
 
-$(window).load(function () {
-    showClientPanel();
-});
+    $(window).load(function () {
+	showClientPanel();
+    });
 
-var clientPanelTemplate;
-var theTipTemplate;
+    var clientPanelTemplate;
+    var theTipTemplate;
 
 })();
