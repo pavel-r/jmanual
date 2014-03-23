8d82442cde90484c658bdb7c2358d945600cf793
Pavel R <rabeckijps@mail.ru>
2014-03-23 21:39:36 +0100

    add authentication

--- web.js
+++ web.js
@@ -3,21 +3,42 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({secret: 'dskltKije34566lkIkd'}));
 app.use('/admin',express.static('admin'));
 app.use('/client',express.static('client'));
 
+var authFunc = function(username, password){
+	return username === 'pavel' && password === '111';
+};
 
 app.get('/', function (request, response) {
-    var content = fs.readFileSync('index.html');
-    response.send(content.toString());
-    console.log('Request processed');
+	if(request.session.username){
+		var content = fs.readFileSync('index.loggedin.html');
+		var contentStr = content.toString();
+		contentStr = contentStr.replace('@user@', request.session.username);
+		response.send(contentStr);
+	} else {
+		var content = fs.readFileSync('index.html');
+		response.send(content.toString());
+	}
+	console.log('Request processed');
 });
 
-app.get('/cors', function (request, response) {
-    console.log('CORS GET Request processed');
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-    response.send("");
+app.post('/', function (request, response) {
+	var username = request.body.email;
+	var password = request.body.password;
+	if(authFunc(username,password)){
+		request.session.username = username;
+		var content = fs.readFileSync('index.loggedin.html');
+		var contentStr = content.toString();
+		contentStr = contentStr.replace('@user@', request.session.username);
+		response.send(contentStr);
+	} else {
+		var content = fs.readFileSync('index.html');
+		response.send(content.toString());
+	}
+	console.log('Request processed');
 });
 
 app.post('/cors', function (request, response) {
@@ -32,16 +53,6 @@ app.post('/cors', function (request, response) {
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
     response.send("[]");
-});
-
-app.post('/', function (request, response){
-    console.log("Data: " + request.body.modelData);
-    var jscontent = fs.readFileSync('jmanual.client.template.js').toString();
-    //check JSON validity
-    jscontent = jscontent.replace("@data@", request.body.modelData);
-    fs.writeFileSync('client/js/jmanual.client.js', jscontent);
-    var content = fs.readFileSync('index.html');
-    response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
