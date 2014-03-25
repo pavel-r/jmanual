b6e79e881d8f46469cb775aa5a985a1fd70d9e41
Pavel R <rabeckijps@mail.ru>
2014-03-25 19:09:21 +0000

    no comment

--- web.js
+++ web.js
@@ -23,7 +23,7 @@ MongoClient.connect(conString, conOptions, function(err, db){
 });
 
 var authFunc = function(email, password){
-    var user = db.collection("users").find({ email : email, password : password});
+    var user = p_db.collection("users").find({ email : email, password : password});
     console.log(user);
     if(user){
 	return true;
